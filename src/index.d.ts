a03b79e3293cb60b2209340501ad41a2a114dbe6
Nguyen Si Hung <github@astecpaints.co.jp>
2022-02-21 17:58:14 +0900

    TS Config

---
+++ src/index.d.ts
@@ -0,0 +1,11 @@
+import React from "react";
+
+export class TextInputKeyboardMangerIOS extends React.Component<any> {}
+
+export class CustomKeyboardView extends React.Component<any> {}
+
+export class KeyboardRegistry extends React.Component<any> {}
+
+export class KeyboardAccessoryView extends React.Component<any> {}
+
+export class KeyboardUtils extends React.Component<any> {}
